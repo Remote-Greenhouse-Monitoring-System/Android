31dbfada3f28392244650685ce848728b9dfdc2e
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2022-11-21 14:07:35 +0100

    tostring

--- app/src/main/java/com/github/group2/android_sep4/main/MainActivity.java
+++ app/src/main/java/com/github/group2/android_sep4/main/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -22,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);  //day
+
         setContentView(R.layout.activity_main);
         preferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
 

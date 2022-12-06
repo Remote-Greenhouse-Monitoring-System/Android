dd743fa8039ba16675e893ec6543ec2b10e299a7
Gruia Pana <gruiapana99@gmail.com>
2022-12-06 21:18:51 +0100

    Plant Profile commit: -Api -Repository -Repository Impl -View Model

--- app/src/main/java/com/github/group2/android_sep4/repository/plant_profile/PlantProfileRepository.java
+++ app/src/main/java/com/github/group2/android_sep4/repository/plant_profile/PlantProfileRepository.java
@@ -1,15 +1,23 @@
 package com.github.group2.android_sep4.repository.plant_profile;
 
+import androidx.lifecycle.MutableLiveData;
+
 import com.github.group2.android_sep4.entity.PlantProfile;
 
 import java.util.List;
 
 public interface PlantProfileRepository {
 
-    PlantProfile createPlantProfile(PlantProfile plantProfile);
-    void removePlantProfile(long id);
-    PlantProfile updatePlantProfile(PlantProfile plantProfile);
-    List<PlantProfile> getPremadePlantProfiles(int pageNumber, int pageSize);
-    PlantProfile getPlantProfileById(long id);
-    void activatePlantProfile(long id);
+    void createPlantProfile(PlantProfile plantProfile);
+    void removePlantProfile(int id);
+    void updatePlantProfile(PlantProfile plantProfile);
+    void getPlantProfilesForUser(int uId);
+    void getPlantProfiles();
+    void getPlantProfileById(int id);
+    void activatePlantProfile(int pId, int gId);
+    void getActivatedPlantProfiles(int gId);
+    MutableLiveData<PlantProfile> getSearchedPlantProfile();
+    MutableLiveData<List<PlantProfile>> getPlantProfilesListForUser();
+    MutableLiveData<List<PlantProfile>> getAllPlantProfilesList();
+    MutableLiveData<List<PlantProfile>> getActivatedPlantProfilesList();
 }
